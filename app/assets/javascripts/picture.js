310e60a943a3d66444a88f9d712d1d206269993c
hpetrow <heather.petrow@gmail.com>
2015-07-23 12:23:24 -0400

    done

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,17 @@
 // Add your JavaScript here
+
+$(function() {
+  $('.like a').click(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var action = $(this).attr('href');
+    var method = $(this).data('method');
+
+    $.ajax({
+      method: method,
+      url: action,
+      dataType: 'script'
+    });
+  });
+});
